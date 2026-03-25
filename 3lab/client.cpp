84dc1981a29c119f91aa08e3ab96baf5113699a7
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2026-03-25 10:21:11 +0300

    Update client.cpp

--- 3lab/client.cpp
+++ 3lab/client.cpp
@@ -39,73 +39,73 @@ void* receive_thread(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-    std::string nickname = "Student";
-    if (argc > 1) nickname = argv[1];
+    std::string nickname = "Student";//задаём имя новоиспечённому 
+    if (argc > 1) nickname = argv[1];//если задано то присвоим его 
 
     while (true) {
-        int sock = ::socket(AF_INET, SOCK_STREAM, 0);
+        int sock = ::socket(AF_INET, SOCK_STREAM, 0);//создаём розетку для связи 
         if (sock < 0) {
-            std::perror("socket");
+            std::perror("socket");//обработка ошибок
             return 1;
         }
 
-        sockaddr_in server_addr{};
-        server_addr.sin_family = AF_INET;
+        sockaddr_in server_addr{};//структура для адресса 
+        server_addr.sin_family = AF_INET;//IPv4
         server_addr.sin_port = htons(SERVER_PORT);
-        inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+        inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);//превращаем текстовый айпи в бинарный вид и закидываем в структуру
 
         std::cout << "Connecting to server...\n";
-        if (::connect(sock, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
+        if (::connect(sock, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {//подключаемся и обрабатываем ошибку 
             std::cout << "Connection failed. Retrying in 2 seconds...\n";
             ::close(sock);
             sleep(2);
-            continue;
+            continue;//пробуем снова прыгаем вначало while пробуем снова достучаться 
         }
 
-        is_connected.store(true);
+        is_connected.store(true);//изменяем переменную что мы подключены 
         std::cout << "Connected!\n";
 
-        send_message(sock, MSG_HELLO, "My name is " + nickname);
+        send_message(sock, MSG_HELLO, "My name is " + nickname);//отправляем серваку привет и свой ник 
 
-        pthread_t recv_tid{};
-        auto* args = new RecvThreadArgs{sock};
-        pthread_create(&recv_tid, nullptr, receive_thread, args);
+        pthread_t recv_tid{};//резервируем айди для потока 
+        auto* args = new RecvThreadArgs{sock};//выделяем память на куче чтобы передать сокет потоку (тот самый args выше) 
+        pthread_create(&recv_tid, nullptr, receive_thread, args);//запускаем поток который слушает сервак и ждёт вывода от польщзозвателя 
 
         std::string input;
         bool quit_requested = false;
-        while (is_connected.load()) {
-            if (!std::getline(std::cin, input)) {
+        while (is_connected.load()) {//пока подключены крутим цикл чтения клваы 
+            if (!std::getline(std::cin, input)) {//если вывод прерван то выходим 
                 quit_requested = true;
                 is_connected.store(false);
                 break;
             }
 
-            if (!is_connected.load()) break;
-            if (input.empty()) continue;
+            if (!is_connected.load()) break;//ну или если флаг подключения фолз то тоже выходим
+            if (input.empty()) continue;//продолжаем если тихо всё
 
-            if (input == "/quit") {
+            if (input == "/quit") {//если пользовать сказал пока то уведомляем сервак и выставляем флаги 
                 send_message(sock, MSG_BYE, "");
                 quit_requested = true;
                 is_connected.store(false);
-                break;
+                break;//выходим
             } else if (input == "/ping") {
-                send_message(sock, MSG_PING, "");
+                send_message(sock, MSG_PING, "");//иначе проверка связи пинго
             } else {
-                send_message(sock, MSG_TEXT, nickname + ": " + input);
+                send_message(sock, MSG_TEXT, nickname + ": " + input);// ну или в конце концов просто отправляем как обычный текст всё остальное 
             }
         }
 
-        ::shutdown(sock, SHUT_RDWR);
-        ::close(sock);
-        pthread_join(recv_tid, nullptr);
+        ::shutdown(sock, SHUT_RDWR);//принудительно обрываем чтение/запись на сокете
+        ::close(sock);//закрываем дескр сокета
+        pthread_join(recv_tid, nullptr);//ждём пока поток чтения реально закончит свою работу и мягко его отпускаем
 
-        if (quit_requested) {
+        if (quit_requested) {//если мы сами вышли через /quit то полностью закрываем прогу  
             std::cout << "Exiting program.\n";
             break;
         }
 
         std::cout << "Reconnecting in 2 seconds...\n";
-        sleep(2);
+        sleep(2);//иначе идём на переподключение 
     }
 
     return 0;
