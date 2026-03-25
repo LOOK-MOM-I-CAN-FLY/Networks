56edd60eb93ef47caa3ffe1b5a841c2e887a8fc4
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2026-03-25 09:35:47 +0300

    Update client.cpp

--- 3lab/client.cpp
+++ 3lab/client.cpp
@@ -7,25 +7,25 @@
 
 #include "protocol.hpp"
 
-std::atomic<bool> is_connected(false);
+std::atomic<bool> is_connected(false);//потокобезопасная переменная один поток может её менять а другой читать без риска ошибок синхронизации
 
 struct RecvThreadArgs {
-    int sock;
+    int sock;//просто структура для передачи в поток дескр сокета 
 };
-
+//работа отдельного потока на стороне клиента, который постоянно слушает сообщения от сервера
 void* receive_thread(void* arg) {
-    auto* args = static_cast<RecvThreadArgs*>(arg);
-    const int sock = args->sock;
-    delete args;
+    auto* args = static_cast<RecvThreadArgs*>(arg);//приводим войд* к нашей структуре чтобы можно прочитать данные
+    const int sock = args->sock;//копируем номер сокета в локальную переменную
+    delete args;//освобождает память выделенную под структуру аргументов 
 
-    Message msg;
-    while (is_connected.load()) {
-        if (!recv_message(sock, msg)) {
+    Message msg;//сюда записываем сообщения 
+    while (is_connected.load()) {//безопасно читаем значение
+        if (!recv_message(sock, msg)) {//вызывем чтение функции сообщения, если получили фолз то ошибка 
             std::cout << "\n[Server disconnected]\n";
-            is_connected.store(false);
+            is_connected.store(false);//меняем на отключение
             break;
         }
-
+        //ну и просто вывод сообщения 
         if (msg.type == MSG_WELCOME) {
             std::cout << "[Server]: " << msg.payload << "\n";
         } else if (msg.type == MSG_TEXT) {
